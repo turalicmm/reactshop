f7aa118f04f20c3ed26489bccc05cf830a64eba2
muca <turaliccmustafa@gmail.com>
2021-06-27 23:42:20 +0200

    first commit

---
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useFormContext, Controller } from "react-hook-form";
+import { TextField, Grid } from "@material-ui/core";
+
+function FormInput({ name, label, required }) {
+  const { control } = useFormContext();
+  const isError = false;
+
+  return (
+    <Grid item xs={12} sm={6}>
+      <>
+        <Controller
+          render={({ field }) => (
+            <TextField {...field} label={label} fullWidth required />
+          )}
+          control={control}
+          name={name}
+        />
+      </>
+    </Grid>
+  );
+}
+
+export default FormInput;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import { Link, useLocation } from "react-router-dom";
+
+import logo from "../../assets/react.jpg";
+import useStyles from "./styles";
+
+const Navbar = ({ totalItems }) => {
+  const location = useLocation();
+  const classes = useStyles();
+
+  return (
+    <AppBar position="fixed" className={classes.appBar} color="inherit">
+      <Toolbar>
+        <Typography
+          component={Link}
+          to="/"
+          variant="h6"
+          className={classes.title}
+          color="inherit"
+        >
+          <img
+            src={logo}
+            alt="Mustafa.shop"
+            height="25px"
+            className={classes.image}
+          />
+          React shop
+        </Typography>
+        <div className={classes.grow} />
+        {location.pathname === "/" && (
+          <div className={classes.button}>
+            <IconButton
+              component={Link}
+              to="/cart"
+              aria-label="Show cart items"
+              color="inherit"
+            >
+              <Badge badgeContent={totalItems} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
